8379b01358cd69b77b321f6ae02ff86311f729a5
Wolgrand Neto <wolgrandneto@MacBook-Air-de-Wolgrand.local>
2019-04-15 00:30:56 -0300

    Adiciona variaveis ambiente

--- src/route.js
+++ src/route.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const multer = require('multer');
+const multerConfig = require('./config/multer');
+
 
 const routes = express.Router();
 
-routes.get('/teste', (req, res) => {
-    return res.send('Hello Worlds ');
-})
+const BoxController = require('./controllers/BoxController');
+const FileController = require('./controllers/FileController');
+
+routes.post("/boxes", BoxController.store);
+routes.get("/boxes/:id", BoxController.show);
+
+routes.post("/boxes/:id/files", multer(multerConfig).single('file'),
+    File Controller.store);
 
 module.exports =  routes;
